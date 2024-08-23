4b9ea9e313f953d64e255337ed7cea9c7831b961
Laura Ricci <lauraricci.lrc@gmail.com>
2024-08-22 21:16:27 -0300

    Create index and profile pages

--- my-app/app/_layout.jsx
+++ my-app/app/_layout.jsx
@@ -1,20 +1,15 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View } from 'react-native'
+import { Slot, Stack } from 'expo-router'
 
-export default function App() {
-    return (
-        <View>
-            <Text>Hello world!</Text>
-            <StatusBar style="auto"/>
-        </View>
-    )
+const RootLayout = () => {
+  return (
+    <Stack>
+        <Stack.Screen 
+            name='index' 
+            options={{ headerShown: false }}
+        />
+    </Stack>
+  )
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
-})
+export default RootLayout
---
+++ my-app/app/index.jsx
@@ -0,0 +1,27 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Link } from "expo-router";
+
+export default function App() {
+    return (
+        <View>
+            <Text>Hello world!</Text>
+            <StatusBar style="auto"/>
+            <Link 
+                href="/profile" 
+                style={{color: 'blue'}}
+            >
+                Profile
+            </Link>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})
