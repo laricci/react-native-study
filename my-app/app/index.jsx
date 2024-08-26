5e3d890031720a6502642d34c34c89cdbbef4422
Laura Ricci <lauraricci.lrc@gmail.com>
2024-08-26 20:44:19 -0300

    Import fonts into layout

--- my-app/app/index.jsx
+++ my-app/app/index.jsx
@@ -5,7 +5,7 @@ import { Link } from "expo-router";
 export default function App() {
     return (
         <View className="flex-1 items-center justify-center bg-white">
-            <Text className="text-3xl">Hello world!</Text>
+            <Text className="text-3xl font-pextrabold">Hello world!</Text>
             <StatusBar style="auto"/>
             <Link 
                 href="/profile" 
