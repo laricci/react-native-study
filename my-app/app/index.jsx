43b5fafc7b3978f81b63dd1f9ba89a6f7384e2ce
Laura Ricci <lauraricci.lrc@gmail.com>
2024-08-26 21:53:58 -0300

    Add navbar

--- my-app/app/index.jsx
+++ my-app/app/index.jsx
@@ -7,11 +7,8 @@ export default function App() {
         <View className="flex-1 items-center justify-center bg-white">
             <Text className="text-3xl font-pextrabold">Hello world!</Text>
             <StatusBar style="auto"/>
-            <Link 
-                href="/profile" 
-                style={{color: 'blue'}}
-            >
-                Profile
+            <Link href="/home" style={{color: 'blue'}}>
+                Home
             </Link>
         </View>
     )
