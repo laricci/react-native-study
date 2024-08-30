ae4e97beffb4de2e4e2b137129a9de66df81142d
Laura Ricci <lauraricci.lrc@gmail.com>
2024-08-29 21:14:58 -0300

    Add basic sign up action with appwrite

---
+++ my-app/lib/appwrite.js
@@ -0,0 +1,65 @@
+import { Account, Avatars, Client, Databases, ID } from 'react-native-appwrite';
+import { config } from './config';
+
+// Init your React Native SDK
+const client = new Client();
+client
+    .setEndpoint(config.appwrite.endpoint) // Your Appwrite Endpoint
+    .setProject(config.appwrite.projectId) // Your project ID
+    .setPlatform(config.appwrite.platform) // Your application ID or bundle ID.
+;
+
+const account   = new Account(client);
+const avatars   = new Avatars(client);
+const databases = new Databases(client);
+
+export const createUser = async (email, password, username) => {
+
+    try {
+
+        const newAccount = await account.create(
+            ID.unique(),
+            email,
+            password,
+            username
+        )
+
+        if (!newAccount) throw Error;
+
+        console.log(newAccount);
+
+        const avatarUrl = avatars.getInitials(username);
+
+        await signIn(email, password);
+
+        const newUser = await databases.createDocument(
+            config.appwrite.databaseId,
+            config.appwrite.userCollectionId,
+            ID.unique(),
+            {
+                accountid: newAccount.$id,
+                email,
+                username,
+                avatar: avatarUrl
+            }
+        );
+
+        return newUser;
+
+    } catch(error) {
+        console.log()
+        throw new Error(error);
+    }
+
+}
+
+export async function signIn(email, password) {
+    try {
+        const session = await account.createEmailPasswordSession(email, password);
+
+    } catch (error) {
+        throw new Error(error);
+    }
+}
+
+
