2ab0608606699ae69dbd94241de582aee47e5e01
Laura Ricci <lauraricci.lrc@gmail.com>
2024-09-04 20:28:49 -0300

    Rectify var name so logged user redirect works properly

--- my-app/app/index.jsx
+++ my-app/app/index.jsx
@@ -11,9 +11,9 @@ import { useGlobalContext } from "../context/GlobalProvider";
 
 export default function App() {
 
-    const {isLoading, isLoggetIn} = useGlobalContext();
+    const {isLoading, isLogged} = useGlobalContext();
 
-    if (!isLoading && isLoggetIn) return <Redirect href="/home" />
+    if (!isLoading && isLogged) return <Redirect href="/home" />
 
     return (
         <SafeAreaView className="bg-primary h-full">
